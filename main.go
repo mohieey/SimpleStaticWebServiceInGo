25f6cf328aa56c6acc277caaee3adbfca171452a
mohieey <Mohhiey@gmail.com>
2023-07-07 10:24:56 +0300

    accept query parameter

--- main.go
+++ main.go
@@ -7,7 +7,13 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
+		names := r.URL.Query()["name"]
+		var name string
+		if len(names) == 1 {
+			name = names[0]
+		}
+
+		w.Write([]byte("hello " + name))
 	})
 
 	err := http.ListenAndServe(":3000", nil)
